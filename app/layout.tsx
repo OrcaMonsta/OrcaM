549e862f3d25c372ba4b5ab585c1dcda53ee76c4
Orca Monsta <OrcaMonsta@gmail.com>
2024-11-11 16:29:29 -0400

    fix: remove all merge conflicts from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import type { Metadata } from 'next'
-<<<<<<< HEAD
 import './globals.css'
 import localFont from 'next/font/local'
 
@@ -10,13 +9,6 @@ const cornerstone = localFont({
   display: 'swap',
   preload: true
 })
-=======
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
->>>>>>> 4ce5a324ecba300cddb21860b7577723c390dafb
 
 export const metadata: Metadata = {
   title: 'Orca Monsta',
@@ -29,18 +21,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-<<<<<<< HEAD
     <html lang="en" className={cornerstone.className}>
       <body>
         <div id="app-root">
           {children}
         </div>
       </body>
-=======
-    <html lang="en">
-      
-      <body>{children}</body>
->>>>>>> 4ce5a324ecba300cddb21860b7577723c390dafb
     </html>
   )
 }
